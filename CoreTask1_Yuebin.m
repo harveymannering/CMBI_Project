845f8273af8b46126a0f06935890bd11d5ff2d2a
YuebinFU <250436510@qq.com>
2022-03-22 00:17:52 +0800

    Simplify code

--- CoreTask1_Yuebin.m
+++ CoreTask1_Yuebin.m
@@ -47,31 +47,19 @@ ylabel('The number of voxels thresholded out')
 connectome_file = {'FA.1_graph.csv'; 'FA.2_graph.csv'; 'FA.3_graph.csv';...
             'FA.4_graph.csv'; 'FA.5_graph.csv'; 'FA.6_graph.csv'; ...
             'FA.7_graph.csv'; 'FA.8_graph.csv'};
+
+density = zeros([1,8]);
+char_path = zeros([1,8]);
+efficiency = zeros([1,8]);
+mean_cluster_coeff = zeros([1,8]);
 for i = 1:8
     fa_file_path = ['connectomes-data/Task1Data/connectomes/', connectome_file{i}];
     % skip the first three lines 
     struct_connect{i} = csvread(fa_file_path,3,0);
-
-    % this function return density, number of vertices, and number of edges in 3
-    [density{i}(1), density{i}(2), density{i}(3)] = density_und(struct_connect{i}); 
-
-    % mean shortest path
-    % distance matrices 
-    distance_mat{i} = distance_bin(struct_connect{i}); 
-    % this function return network characteristic path length and network global efficiency
-    % Characteristic path length is defined here as the mean shortest path length between all pairs of nodes
-    % About missing edges and disconnected, it will be infinite value, it
-    % would be zero as third argument setting
-    [char_path{i}(1), char_path{i}(2)]  = charpath(distance_mat{i},0,0); 
-
-    % efficiency
-    efficiency{i} = 1 / efficiency_bin(struct_connect{i}); 
-    
-    % mean clustering coefficient
-    mean_cluster_coeff{i} = mean(clustering_coef_bu(struct_connect{i})); 
+    [density(i),char_path(i),efficiency(i),mean_cluster_coeff(i)] = graph_metrics(struct_connect{i});
 end
-
-display_graphs(density,char_path,efficiency,mean_cluster_coeff,'FA Threshold');
+display_graphs(density,char_path,efficiency,mean_cluster_coeff, ...
+    'FA Threshold','When FA threshold is changing:');
 
 %% question 4
 parcellation_func = load_nii('connectomes-data/Task1Data/tractor/functional/parcellation.nii.gz');
@@ -89,7 +77,7 @@ avg_time_series = zeros(num_regions, 15);
 for i = 1:num_regions
     id = region_ids(i);
 
-    % use mask to select voxels 
+    % use mask to select voxels of this cortical region
     cortical_region = (parcellation_func.img == id); 
     voxels = data_func.img(cortical_region(:,:,:,ones(1,15)));
     [size_voxels,~] = size(voxels); 
@@ -104,7 +92,7 @@ for i = 1:8
     corshrink_matrix{i} = corshrink(avg_time_series',FA_threshold(i));
 end
 
-% resulting matrix at a correlation of 0.1, binarise it
+% resulting matrics at a correlation of 0.1, binarise it
 for t = 1:8
     corshrink_new = corshrink_matrix{t};
     % binarise it
@@ -114,16 +102,17 @@ for t = 1:8
 end
 
 %  graph metrics 
+cor_density = zeros([1,8]);
+cor_char_path = zeros([1,8]);
+cor_efficiency = zeros([1,8]);
+cor_mean_cluster_coeff = zeros([1,8]);
 for i = 1:8
-    % function like q3
-    [cor_density{i}(1), cor_density{i}(2), cor_density{i}(3)] = density_und(binary_corshrink{i}); 
-    cor_dist_mat{i} = distance_bin(binary_corshrink{i}); 
-    [cor_char_path{i}(1), cor_char_path{i}(2)]  = charpath(cor_dist_mat{i},0,0); 
-    cor_efficiency{i} = 1 / efficiency_bin(binary_corshrink{i}); 
-    cor_mean_cluster_coeff{i} = mean(clustering_coef_bu(binary_corshrink{i})); 
+    [cor_density(i),cor_char_path(i),cor_efficiency(i),cor_mean_cluster_coeff(i)] = ...
+        graph_metrics(binary_corshrink{i});
 end
 
-display_graphs(cor_density,cor_char_path,cor_efficiency,cor_mean_cluster_coeff,'Lambda');
+display_graphs(cor_density,cor_char_path,cor_efficiency,cor_mean_cluster_coeff ...
+    ,'Lambda','When negative correlations of |0.1| are retained:');
 
 % discarding negative correlations with absolute value greater than 0.1
 for t = 1:8
@@ -132,15 +121,17 @@ for t = 1:8
     discard_neg_corshrink{t} = corshrink_new;
 end
 
+discard_neg_density = zeros([1,8]);
+discard_neg_char_path = zeros([1,8]);
+discard_neg_efficiency = zeros([1,8]);
+discard_neg_mean_cluster_coeff = zeros([1,8]);
 for i = 1:8
-    [discard_neg_density{i}(1), discard_neg_density{i}(2), discard_neg_density{i}(3)] = density_und(discard_neg_corshrink{i});
-    discard_neg_dist_mat{i} = distance_bin(discard_neg_corshrink{i}); 
-    [discard_neg_char_path{i}(1), discard_neg_char_path{i}(2)]  = charpath(discard_neg_dist_mat{i},0,0); 
-    discard_neg_efficiency{i} = 1 / efficiency_bin(discard_neg_corshrink{i}); 
-    discard_neg_mean_cluster_coeff{i} = mean(clustering_coef_bu(discard_neg_corshrink{i})); 
+    [discard_neg_density(i),discard_neg_char_path(i),discard_neg_efficiency(i),...
+        discard_neg_mean_cluster_coeff(i)] = graph_metrics(discard_neg_corshrink{i});
 end
 
-display_graphs(discard_neg_density,discard_neg_char_path,discard_neg_efficiency,discard_neg_mean_cluster_coeff,'Lambda');
+display_graphs(discard_neg_density,discard_neg_char_path,discard_neg_efficiency, ...
+    discard_neg_mean_cluster_coeff,'Lambda','When negative correlations of |0.1| are discarded:');
 
 % function definition
 % mannually setting lambda
@@ -180,33 +171,45 @@ function xc = makeStdOne(x)
     xc = x ./ sd; 
 end
 
-function display_graphs(density,char_path,efficiency,mean_cluster_coeff,x_label)
+function display_graphs(density,char_path,efficiency,mean_cluster_coeff,x_label,title_gigure)
     FA_threshold = [0.1:0.1:0.8];
-    density_data = zeros([1,8]);
-    mean_shortest_path = zeros([1,8]);
-    efficiency_data = zeros([1,8]);
-    mean_cluster_coeff_data = zeros([1,8]);
-    for i = 1:8
-        density_data(i) = density{i}(1);
-        mean_shortest_path(i) = char_path{i}(1);
-        efficiency_data(i) = efficiency{i};
-        mean_cluster_coeff_data(i) = mean_cluster_coeff{i};
-    end
 
     figure
     hold on 
-    plot(FA_threshold,mean_shortest_path)
+    plot(FA_threshold,char_path)
     hold on 
-    plot(FA_threshold,efficiency_data)
+    plot(FA_threshold,efficiency)
     xlabel(x_label)
     ylabel('Value')
     legend('mean shortest path','efficiency')
+    title(title_gigure)
 
     figure
-    plot(FA_threshold,density_data)
+    plot(FA_threshold,density)
     hold on 
-    plot(FA_threshold,mean_cluster_coeff_data)
+    plot(FA_threshold,mean_cluster_coeff)
     xlabel(x_label)
     ylabel('Value')
     legend('edge density','mean clustering coefficient')
+    title(title_gigure)
+end
+
+function [density,char_path,efficiency,mean_cluster_coeff] = graph_metrics(input)
+    % this function return density, number of vertices, and number of edges in 3
+    [density,~,~] = density_und(input); 
+
+    % mean shortest path
+    % distance matrices 
+    distance_mat = distance_bin(input); 
+    % this function return network characteristic path length and network global efficiency
+    % Characteristic path length is defined here as the mean shortest path length between all pairs of nodes
+    % About missing edges and disconnected, it will be infinite value, it
+    % would be zero as third argument setting
+    [char_path, ~]  = charpath(distance_mat,0,0); 
+
+    % efficiency
+    efficiency = 1 / efficiency_bin(input); 
+    
+    % mean clustering coefficient
+    mean_cluster_coeff = mean(clustering_coef_bu(input)); 
 end

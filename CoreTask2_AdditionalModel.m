93e431dae9889621e328059bb714294b472a7865
ZiweiChen <92172395+KANI00@users.noreply.github.com>
2022-03-22 10:10:32 +0000

    Update CoreTask2_AdditionalModel.m

--- CoreTask2_AdditionalModel.m
+++ CoreTask2_AdditionalModel.m
@@ -33,6 +33,89 @@ for p = 1:size(s,3)
     end 
 end 
 
+%% Additional model 9 f = alpha + beta * t + gamma * s.^2
+alpha9 = zeros(size(s,1),size(s,2)); 
+beta9 = zeros(size(s,1),size(s,2)); 
+gamma9 = zeros(size(s,1),size(s,2)); 
+
+for i = 1:size(s, 1)
+    for j = 1:size(s, 2)
+        s_slice = reshape(s(i,j,:), 19, 1);
+        t_slice = reshape(t(i,j,:), 19, 1);
+        X = [ones(19, 1) t_slice (s_slice.^2)];
+        Y = reshape(f(i,j,:), 19, 1);
+
+        coeff = inv(X' * X) * X' * Y;
+        alpha9(i,j) = coeff(1);
+        beta9(i,j) = coeff(2);
+        gamma9(i,j) = coeff(3);
+    end
+end
+
+SSE9 = zeros(19,1);
+for k = 1:19
+    model9_sum = zeros(68);
+    res9 = f(:,:,k) - (alpha9 + beta9.*t(:,:,k) + gamma9.*s(:,:,k).^2);
+    model9_sum = model9_sum + res9.^2;
+    model9_sum(isinf(model9_sum)) = 0;
+    model9_sum(isnan(model9_sum)) = 0;
+    SSE9(k) = sum(sum(model9_sum));
+end
+
+best_SSE9 = realmax;
+best_alpha9 = zeros(size(s,1), size(s,2));
+best_beta9 = zeros(size(s,1), size(s,2));
+best_gamma9 = zeros(size(s,1), size(s,2));
+best_aic9 = 0;
+best_bic9 = 0;
+
+for cv = 1:19
+    f_test = f(:,:,cv);
+    f_train = f(:,:,[1:cv-1,cv+1:19]);
+    s_test = s(:,:,cv);
+    s_train = s(:,:,[1:cv-1,cv+1:19]);
+    t_test = t(:,:,cv);
+    t_train = t(:,:,[1:cv-1,cv+1:19]);
+    
+    cv_alpha9 = zeros(size(s,1),size(s,2));
+    cv_beta9 = zeros(size(s,1),size(s,2));
+    cv_gamma9 = zeros(size(s,1),size(s,2));
+    
+    for i = 1:size(s, 1)
+        for j = 1:size(s, 2)
+            s_slice = reshape(s_train(i,j,:), 18, 1);
+            t_slice = reshape(t_train(i,j,:), 18, 1);
+            cv_X = [ones(18, 1) t_slice (s_slice.^2)];
+            cv_Y = reshape(f_train(i,j,:), 18, 1);
+
+            cv_coeff = inv(cv_X' * cv_X) * cv_X' * cv_Y;
+            cv_alpha9(i,j) = cv_coeff(1);
+            cv_beta9(i,j) = cv_coeff(2); 
+            cv_gamma9(i,j) = cv_coeff(3);
+
+        end
+    end
+    
+    cv_model9_sum = zeros(68);
+    cv_res9 = f_test - (cv_alpha9 + cv_beta9.*t_test + cv_gamma9.*s_test.^2);
+    cv_model9_sum = cv_model9_sum + cv_res9.^2;
+    cv_model9_sum(isinf(cv_model9_sum)) = 0;
+    cv_model9_sum(isnan(cv_model9_sum)) = 0;
+    cv_SSE = sum(cv_model9_sum,'all');
+    
+    aic9 = 2*4 + 19.* log(cv_model9_sum./19);
+    bic9 = 4*log(19) + 19.*log(cv_model9_sum./19);
+    
+    if cv_SSE < best_SSE9
+        best_SSE9 = cv_SSE;
+        best_alpha9 = cv_alpha9;
+        best_beta9 = cv_beta9;
+        best_gamma9 = cv_gamma9;
+        best_aic9 = MeanExcludingInf(aic9);
+        best_bic9 = MeanExcludingInf(bic9);
+    end
+end  
+
 %% Additional model 6 f = alpha + beta * t.^3
 alpha6 = zeros(size(s,1),size(s,2)); 
 beta6 = zeros(size(s,1),size(s,2)); 
@@ -208,7 +291,7 @@ gamma8 = zeros(size(s,1),size(s,2));
 for i = 1:size(s, 1)
     for j = 1:size(s, 2)
         s_slice = reshape(s(i,j,:), 19, 1);
-        t_slice = reshape(s(i,j,:), 19, 1);
+        t_slice = reshape(t(i,j,:), 19, 1);
         X = [ones(19, 1) s_slice (t_slice.^2)];
         Y = reshape(f(i,j,:), 19, 1);
 
@@ -265,29 +348,18 @@ for cv = 1:19
     cv_SSE8(cv) = sum(cv_model8_sum,'all');
 end    
 
-% plot the relationship
-% figure; 
-% for i=1:10   
-%     position = randi([1,68],2,1);
-%     fval = [];
-%     sval = [];
-%     tval = [];
-%     for k = 1:19
-%         fval = [fval,f(position(1),position(2),k)];
-%         sval = [sval,s(position(1),position(2),k)];
-%         tval = [tval,t(position(1),position(2),k)];
-%     end
-%     
-%     subplot(2,5,i);
-%     x = linspace(0,0.7);
-%     y = linspace(0,0.7);
-%     a = cv_alpha8(position(1),position(2));
-%     b = cv_beta8(position(1),position(2));
-%     c = cv_gamma8(position(1),position(2));
-%     plot3(x,y,a+b.*x+c.*y.^2);
-%     grid on;
-%     hold on;
-%     
-%     scatter3(sval,tval,fval);
-%     title(sprintf('(%.0f,%.0f)', position(1),position(2)));
-% end
+%%
+function m = MeanExcludingInf(matrix)
+    
+    sum = 0;
+    count = 0;
+    for i=1:size(matrix,1)
+        for j=1:size(matrix,2)
+            if isinf(matrix(i,j)) == 0
+                sum = sum + matrix(i,j);
+                count = count + 1;
+            end
+        end
+    end
+    m = sum / count;
+end
